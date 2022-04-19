620524103495d752abea9b7265f9d8a1c4bcf923
omar-etman <omartareketman@gmail.com>
2022-04-20 01:00:14 +0200

    build and style the contactCard component & initiate the homepage setup

--- connect-chat-app/src/Components/ContactCard.tsx
+++ connect-chat-app/src/Components/ContactCard.tsx
@@ -8,17 +8,8 @@ function ContactCard() {
   return (
     <div className='contactCard-container'>
         <div className='contactCard-profilePic'>
-            <Badge
-                overlap="circular"
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                variant="dot"
-            >
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
             </Badge>
-        </div>
-        <div className='contactCard-info'>
-            <p className='contacrCard-name'>Contact's name</p>
-            <p className='contactCard-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quia dolorum amet porro facere. Cupiditate, est quasi itaque necessitatibus at numquam, perferendis blanditiis inventore deserunt accusantium esse ex. Omnis, atque.</p>
         </div>
     </div>
   )
