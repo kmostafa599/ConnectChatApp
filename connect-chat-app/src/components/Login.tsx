967faede550cfc30ee8d4f62b6b920819c752f98
Kareem Mostafa <kmostafa599@gmail.com>
2022-04-19 13:38:15 +0200

    LoginPage

---
+++ connect-chat-app/src/components/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () : JSX.Element => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
---
+++ connect-chat-app/src/components/SignUp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () : JSX.Element => {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignUp
