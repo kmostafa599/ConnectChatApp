49a9c2bbee39c4146f63ffd7a915a5c6f94b685b
omar-etman <omartareketman@gmail.com>
2022-04-21 04:24:25 +0200

    add the mobile chat header back button + css mod

--- connect-chat-app/src/Components/ChatHeader.tsx
+++ connect-chat-app/src/Components/ChatHeader.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Avatar } from '@mui/material'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 function ChatHeader() {
   return (
     <div className='contacts-header'>
         <div className='header-contact-id'>
-            <Avatar/>
-            <h1>Contact's Name</h1>
+          <ArrowBackIosIcon/>
+          <Avatar/>
+          <h1>Contact's Name</h1>
         </div>
     </div>
   )
